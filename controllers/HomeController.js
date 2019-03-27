3de01e0146985862afc343f6c1aeaddc50118084
DKrakowczyk <dawid.krakowczyk.1996@gmail.com>
2019-03-27 23:05:29 +0100

    home debug

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -17,19 +17,20 @@ exports.homePage = (req, res) => {
     User.find((err, users) => {
         if (!err) {
             fs.readdir('./public/uploads', (err, files) => {
-                if (files !== undefined) {
                     files.forEach(file => {
                         photoArray.push(file);
                     });
                     if (users.length > 0) {
+                        console.log("Znalazlem uzytkownika - home");
                         res.render("home", { photoArray: photoArray, photo: users[0].imagePath, username: users[0].username, desc: users[0].description });
                     } else {
+                        console.log("Nie znalazlem uzytkownika - login");
                         res.render("home", {
                             photoArray: [], photo: "images/avatars/default.jpg", username: "Default user", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis lacinia quam, sit amet pulvinar nisl volutpat id. Aenean non semper tellus, sed malesuada augue. Nullam posuere rutrum mi in scelerisque. Nulla ex nunc, feugiat eget diam at, fermentum sagittis dolor. Proin tempor augue vitae dui suscipit efficitur."
                         });
                     }
                 }
-            });
+            );
         } else {
             res.render("home", {
                 photoArray: [], photo: "images/avatars/default.jpg", username: "Default user", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis lacinia quam, sit amet pulvinar nisl volutpat id. Aenean non semper tellus, sed malesuada augue. Nullam posuere rutrum mi in scelerisque. Nulla ex nunc, feugiat eget diam at, fermentum sagittis dolor. Proin tempor augue vitae dui suscipit efficitur."
@@ -44,9 +45,10 @@ exports.loginPage = (req, res) => {
         User.find((err, users) => {
             if (!err) {
                 if (users.length > 0) {
-                    console.log("Znalazlem uzytkownika");
+                    console.log("Znalazlem uzytkownika - login");
                     res.render("login", { isRegistered: 1, accountType: users[0].accountType });
                 } else {
+                    console.log("Nie znalazlem uzytkownika - login");
                     res.render("login", { isRegistered: 0 });
                 }
             } else {
